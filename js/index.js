5423c65d322b3c399f970762250d64bcfa78963a
Coty Embry <cotyembry@gmail.com>
2017-04-27 18:52:52 -0500

    updating files

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ import App from './components/App.jsx';
 	HoverBubbleContainer.id  = 'HoverBubbleContainerId';
 	HoverBubbleContainer.style = 'width: 100%; height: 100%; position: absolute; top: 0px;';
 	document.body.appendChild(HoverBubbleContainer);
-	ReactDOM.render(<App />, document.getElementById('HoverBubbleContainerId'))
+	ReactDOM.render(<App />, document.getElementById('HoverBubbleContainerId'));
 })();
 
 
